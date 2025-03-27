536edaa8687e94def83ebc312f673c0adaaf0305
Ashish Vishwakarma <142133424+wwwashishtech@users.noreply.github.com>
2025-03-28 00:56:02 +0530

    Update panda_ai.py

--- panda_ai.py
+++ panda_ai.py
@@ -20,7 +20,7 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from geopy.geocoders import Nominatim
 from timezonefinder import TimezoneFinder
-import pywhatkit as kit
+
 import pyjokes
 import wolframalpha
 import subprocess
